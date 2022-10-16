6b27ba88cbc2e92756a69af794e8ef5a22df1a3e
samiam2013 <sam@myres.dev>
2022-10-16 20:10:18 +0000

    add sum series solutions

---
+++ learn-rust/src/1-elementary/4-sum-series.rs
@@ -0,0 +1,14 @@
+// Modify the previous program such that only multiples of three or five 
+//  are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
+use std::io::stdin;
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    input.truncate(input.trim().len());
+    let conv_num = input.parse::<i64>();
+    let num_int = conv_num.unwrap();
+    println!("converted number: {:?}", num_int);
+    let triangle = (num_int * (num_int + 1))/2 ;
+    println!("triangle sum: {:?}", triangle);
+
+}
---
+++ learn-rust/src/1-elementary/5-fizzbuzz-triangle.rs
@@ -0,0 +1,19 @@
+// Write a program that asks the user for a number n and prints
+//  the sum of the numbers 1 to n
+use std::io::stdin;
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    input.truncate(input.trim().len());
+    println!("some input: {:?}", input);
+    let conv_num = input.parse::<i64>();
+    let num_int = conv_num.unwrap();
+    println!("converted number: {:?}", num_int);
+    let mut sum: i64 = 0;
+    for i in 1..num_int {
+        if i == 3 || i == 5 {
+            sum += i;
+        }
+    }
+    println!("fizzbuzz triangle sum: {:?}", sum);
+}
