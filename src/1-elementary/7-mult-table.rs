94d6458641556c68bc69912ef471585452f195a5
samiam2013 <sam@myres.dev>
2022-10-19 06:28:52 +0000

    make a multiplication table

---
+++ src/1-elementary/7-mult-table.rs
@@ -0,0 +1,21 @@
+
+fn main() {
+    for x in 1..13 {
+        for y in 1..13 {
+            let prod: i64 = x * y;
+            print!(" {:>3}",prod);
+        }
+        println!()
+    }
+}
+
+// go version
+// func MultiplicationTable() {
+// 	for row := 1; row <= 12; row++ {
+// 		for column := 1; column <= 12; column++ {
+// 			product := row * column
+// 			fmt.Printf("%4v", product)
+// 		}
+// 		fmt.Print("\n")
+// 	}
+// }
