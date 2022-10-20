64eaa2dc85c2fddd77c9f4244e1adfc30a956063
samiam2013 <sam@myres.dev>
2022-10-20 04:01:35 +0000

    copy over more instructions

---
+++ src/1-elementary/10-next-twenty-ly.rs
@@ -0,0 +1,1 @@
+// 10. Write a program that prints the next 20 leap years.
---
+++ src/1-elementary/11-alternating-series.rs
@@ -0,0 +1,2 @@
+// 11. Write a program that computes the sum of an alternating series where each element of the series 
+// is an expression of the form ((-1)^{k+1})/(2 * k-1) for each value of k from 1 to a million, multiplied by 4.d
