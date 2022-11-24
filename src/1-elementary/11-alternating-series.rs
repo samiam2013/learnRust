b2ab4c8666799d21057655b44413a28983f29890
samiam2013 <sam@myres.dev>
2022-11-24 04:11:26 +0000

    houskeeping!

--- src/1-elementary/11-alternating-series.rs
+++ src/1-elementary/11-alternating-series.rs
@@ -1,4 +1,4 @@
-// 11. Write a program that computes the sum of an alternating series where each element of the series 
+// 11. Write a program that computes the sum of an alternating series where each element of the series
 // is an expression of the form ((-1)^{k+1})/(2 * k-1) for each value of k from 1 to a million, multiplied by 4.d
 
 fn main() {
--- src/2-intermediate/2-compute-leaps.rs
+++ src/2-intermediate/2-compute-leaps.rs
@@ -1,13 +1,11 @@
 // 2. Write a program that takes the duration of a year (in fractional days) for an imaginary planet
 //  as an input and produces a leap-year rule that minimizes the difference to the planet’s solar year.
 
-
 fn main() {
     let rules = compute_ly_rule(365.2422);
     for rule in rules {
-        println!("{:}",rule);
+        println!("{:}", rule);
     }
-
 }
 
 fn compute_ly_rule(days: f64) -> Vec<String> {
@@ -22,12 +20,12 @@ fn compute_ly_rule(days: f64) -> Vec<String> {
         }
         if extra_day >= ((1.0 / i as f64) - ((1.0 / i as f64) * 0.1)) {
             rules.push(format!("must have a leap year every {:} years", i));
-            extra_day -= 1.0/i as f64;
+            extra_day -= 1.0 / i as f64;
             last_freq = i;
         }
         if extra_day <= -((1.0 / i as f64) + ((1.0 / i as f64) * 0.1)) {
             rules.push(format!("must remove a leap year every {:} years", i));
-            extra_day += 1.0/i as f64;
+            extra_day += 1.0 / i as f64;
             last_freq = i;
         }
     }
