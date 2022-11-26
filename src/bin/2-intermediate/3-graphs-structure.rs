12c2a56fab7100a5ecc06d68c8e587540cf11364
samiam2013 <sam@myres.dev>
2022-11-26 18:41:39 +0000

    whomegacursed

--- src/bin/2-intermediate/3-graphs-structure.rs
+++ src/bin/2-intermediate/3-graphs-structure.rs
@@ -1,51 +1,36 @@
-// 3. Implement a data structure for graphs that allows modification (insertion, deletion).
-//  It should be possible to store values at edges and nodes.
-//  It might be easiest to use a dictionary of (node, edgelist) to do this.
+use std::sync::{Arc, Mutex};
+
+struct Graph<'a, T> {
+    root: &'a Arc<Mutex<Node<'a,T>>>,
+}
+
+fn add_node<'a,T>(g: Graph<'a,T>, edge_val: T, n:&'a Arc<Mutex<Node<'a,T>>>) -> Graph<'a,T> {
+    let new_node:&'a Arc<Mutex<Node<'a, T>>> = n;
+    let mut m= g.root.lock().unwrap();
+    m.edges.push(Edge{ value: edge_val, from: &g.root, to: &new_node });
+    g
+}
 
 // node contains a value and list of 'edges' pointing to other nodes
 #[allow(dead_code)]
 #[derive(Debug)]
-struct Node<T> {
+struct Node<'a,T> {
     value: T,
-    edges: Vec<Edge<T>>,
+    edges: Vec<Edge<'a,T>>,
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
-struct Edge<T> {
+struct Edge<'a, T> {
     value: T,
-    from: Node<T>,
-    to: Node<T>,
+    from: &'a Arc<Mutex<Node<'a, T>>>,
+    to: &'a Arc<Mutex<Node<'a,T>>>,
 }
 
 fn main() {
-    let mut graph: Vec<Node<String>> = Vec::new();
-    graph.push(Node {
-        value: String::from("something"),
-        edges: vec![
-            Edge {
-                value: String::from("foo"),
-                from: Node {
-                    value: String::from("node"),
-                    edges: Vec::new(),
-                },
-                to: Node {
-                    value: String::from("node2"),
-                    edges: Vec::new(),
-                },
-            },
-            Edge {
-                value: String::from("bar"),
-                from: Node {
-                    value: String::from("baz"),
-                    edges: Vec::new(),
-                },
-                to: Node {
-                    value: String::from("baz2"),
-                    edges: Vec::new(),
-                },
-            },
-        ],
-    });
-    println!("{:?}", graph);
+    let n: Arc<Mutex<Node<String>>> = Arc::new(
+        Mutex::new(Node { value: (&"node_val").to_string(), edges: Vec::new() })
+    );
+    let g: Graph<String> = Graph { root: &n };
+    add_node(g, "edge_val".to_string(), &n);
 }
