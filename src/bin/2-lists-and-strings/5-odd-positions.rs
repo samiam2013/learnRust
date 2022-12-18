088b9709ad14a00efe5b58946088214d9d7935c0
samiam2013 <sam@myres.dev>
2022-12-18 23:33:31 +0000

    return odd positions of list

--- src/bin/2-lists-and-strings/5-odd-positions.rs
+++ src/bin/2-lists-and-strings/5-odd-positions.rs
@@ -2,17 +2,17 @@
 
 fn main() {
     // starting from 0th index, by index oddity
-    let list = vec!("even0", "odd1", "even2", "odd3");
+    let list = vec!["even0", "odd1", "even2", "odd3"];
     println!("odd items: {:?}", odd_pos(list))
 }
 
 fn odd_pos<T: std::marker::Copy>(list: Vec<T>) -> Vec<T> {
-    let mut odds = vec!();
-    let mut i = 1; 
+    let mut odds = vec![];
+    let mut i = 1;
     while i < list.len() {
         let odd_val = list[i];
         odds.push(odd_val);
         i += 2;
     }
-    return odds
+    return odds;
 }
