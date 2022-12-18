78d8835be964d995ed2286ff420977ab751d5417
samiam2013 <sam@myres.dev>
2022-12-18 22:23:51 +0000

    add largest element search

---
+++ src/bin/2-lists-and-strings/2-largest-element.rs
@@ -0,0 +1,19 @@
+/* 2. Write a function that returns the largest element in a list. */
+
+fn main() { 
+    let list: Vec<String> = [
+        "item1".to_string(), 
+        "longest item".to_string()].to_vec();
+    let longest = find_longest(list);
+    println!("longest item: {:}", longest)
+}
+
+fn find_longest(list: Vec<String>) -> String {
+    let mut longest: String = String::new();
+    for li in list {
+        if li.len() > longest.len() {
+            longest = li
+        }
+    }
+    longest
+}
